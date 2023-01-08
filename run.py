2403daf1c0af5c3df54ce478e16a2c3157e0141b
DogeDevYT <52612977+DogeDevYT@users.noreply.github.com>
2023-01-08 11:02:52 -0500

    Added Login credentials, results serialization, ability to pick what courses to save, and fixed the lowest grade calculation (there was a slight issue))
    -Added login credentials file that holds your Infinite Campus login details but you have to add it yourself (just make it)
    start_of_file:
    <ic username>
    <ic password>
    end_of_file:
    -Added the serialization of results with the format:
    course name, current grade, desired lowest grade, lowest summative, lowest formative, current summatives (its the array you inputed), and current formatives (once again the array you inputted)
    -Gave the user some choice with what courses to save/calculate for instead of going through all of them iteratively
    -The lowest grade calculation wasn't working correctly because I combined them and averaged the total (not what was supposed to happen) so instead I fixed it by taking the array by itself instead of averaging it out initially

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from collections import defaultdict
 from statistics import mean
+from os.path import exists
 import time
+import csv
 
 summative_weighting_factor = 0.75
 formative_weighting_factor = 0.25
+
 
 # main entering grades loop function
 def grade_loop(grade_list):
@@ -21,43 +23,76 @@ def grade_loop(grade_list):
 
 # function to calculate preEOC Average (also works for non EOC courses since EOC score is averaged with pre eoc average)
 def average(summatives, formatives):
-    #https://www.wikihow.com/Calculate-Weighted-Average
+    # https://www.wikihow.com/Calculate-Weighted-Average
     return (mean(summatives) * summative_weighting_factor) + (
-                mean(formatives) * formative_weighting_factor)
+            mean(formatives) * formative_weighting_factor)
 
 
 # function to calculate the lowest grade possible and still get the desired score
 def lowest_grade(desiredGrade, summatives, formatives):
-    current_grade = average(summatives, formatives)
-    #(current_grade + x)/2 = desiredGrade
-    #current grade + x = 2(desiredGrade)
-    # x = 2(desiredGrade) - current grade
-    missing_grade = (2 * desiredGrade) - current_grade
-    missing_grade_summative = missing_grade * summative_weighting_factor
-    missing_grade_formative = missing_grade * formative_weighting_factor
-    result = [missing_grade_summative, missing_grade_formative]
-    return result
+    #(sum(summatives) + x)/(len(summatives)+1) = desiredGrade
+
+    missing_grade_summative = ((desiredGrade * (len(summatives)+1)) - sum(summatives))
+    missing_grade_formative = ((desiredGrade * (len(formatives)+1)) - sum(formatives))
+    return [missing_grade_summative, missing_grade_formative]
 
 
 # abstracts the pre EOC grade calculating process to make it easier for me to code
 def calculate_grade_final(summatives, formatives, course):
     print('Please enter your FORMATIVE grades:')
     formatives = grade_loop(formatives)
-    #formatives = [int(numeric_string) for numeric_string in formatives] #convert into integer list
+    # formatives = [int(numeric_string) for numeric_string in formatives] #convert into integer list
     print('FORMATIVES for ' + str(course) + ':' + str(formatives))
     print('Please enter your SUMMATIVE grades:')
     summatives = grade_loop(summatives)
-    #summatives = [int(numeric_string) for numeric_string in summatives] #convert into integer list
+    # summatives = [int(numeric_string) for numeric_string in summatives] #convert into integer list
     print('SUMMATIVES for ' + str(course) + ':' + str(summatives))
     # https://www.wikihow.com/Calculate-Weighted-Average
     average_grade = average(summatives, formatives)
     print('Average for ' + str(course) + ':' + str(average_grade))
-    desired_end_grade = int(input('What is your desired end grade for ' + str(course) + '? '))
-    #TODO: implement logic for determining the lowest grade I can get and still have a desired_end_grade also remember to change desired end grade to actually be right (reasonable answer come on)
+    desired_end_grade = int(input('What is your LOWEST acceptable end grade for ' + str(course) + '? '))
     lowest_grade_possible = lowest_grade(desired_end_grade, summatives, formatives)
+    #print(lowest_grade_possible)
+    print('For ' + str(course) + ' the lowest grade you can get on a summative is a ' + str(lowest_grade_possible[0]) + ' and the lowest grade you can get on a formative is a ' + str(lowest_grade_possible[1]) + ' to get a ' + str(desired_end_grade) + ' in the course')
+    save = input('Would you like to save these results? (y/n)')
+    if save == 'yes' or save == 'y':
+        if (exists('results.csv')):
+            with open('results.csv', 'a') as results: # if the file already exists I just append to the end of it
+                # with new data instead of overwriting with write file mode
+                writer = csv.writer(results)
+                # Format: course name, current grade, desired lowest grade, lowest summative, lowest formative,
+                # current summatives
+                writer.writerow(
+                    [str(course), average_grade, desired_end_grade, lowest_grade_possible[0], lowest_grade_possible[1],
+                     summatives, formatives])
+                print('Results successfully written!')
+        else:
+            with open('results.csv', 'w') as results:
+                writer = csv.writer(results)
+                # Format: course name, current grade, desired lowest grade, lowest summative, lowest formative,
+                # current summatives
+                writer.writerow(
+                    ['Course Name', 'Current Grade', 'Desired Lowest Grade', 'Lowest Summative', 'Lowest Formative',
+                     'Current Summatives', 'Current Formatives'])
+                writer.writerow(
+                    [str(course), average_grade, desired_end_grade, lowest_grade_possible[0], lowest_grade_possible[1],
+                     summatives, formatives])
+                print('Results successfully written!')
+    else:
+        print('Ok...Not saving results for ' + str(course))
+
+
+login_credentials = []
+
+with open('login.txt', 'r') as login_file:
+    for line in login_file:
+        if line == '\n':  # checking for empty lines which would mean a missing username/password
+            exit('Missing/improper configuration of login.txt, Aborting...')
+        login_credentials.append(line.strip())  # getting rid of newline character
 
 # Create a webdriver object (basically the browser to use)
-driver = webdriver.Chrome()
+# REMEMBER TO DOWNLOAD CHROMEDRIVER EXECUTIBLE AND PLACE IT IN THE SAME DIRECTORY AS THE PYTHON SCRIPT!!
+driver = webdriver.Chrome(executable_path='chromedriver.exe')
 
 # Navigate to the webpage containing the button
 driver.get('https://campus.forsyth.k12.ga.us/campus/portal/students/forsyth.jsp')
@@ -65,10 +100,11 @@ driver.get('https://campus.forsyth.k12.ga.us/campus/portal/students/forsyth.jsp'
 # Locate the button by its ID and click on it to start SSO process
 driver.find_element_by_id('samlLoginLink').click()
 
-# wait so that user can input their login data
-time.sleep(10)
-
-# find login button and login
+# enter login data and submit
+driver.find_element_by_id('userNameInput').send_keys(login_credentials[0])
+time.sleep(1)
+driver.find_element_by_id('passwordInput').send_keys(login_credentials[1])
+time.sleep(1)
 driver.find_element_by_id('submitButton').click()
 
 # wait for load
@@ -94,6 +130,7 @@ with open("query.txt") as query_file:
         funky_url_parameters += line
         funky_url_parameters += "&"
 
+
 ################################
 #           TESTING            #
 #                              #
@@ -102,17 +139,25 @@ with open("query.txt") as query_file:
 ################################
 
 # make final query
-#final_query = 'https://campus.forsyth.k12.ga.us/campus/nav-wrapper/student/portal/student/classroom/grades/student-grades?' + funky_url_parameters + "classroomSectionID=" + str(courses["AP American Lit Lang/Comp"])
+# final_query = 'https://campus.forsyth.k12.ga.us/campus/nav-wrapper/student/portal/student/classroom/grades/student-grades?' + funky_url_parameters + "classroomSectionID=" + str(courses["AP American Lit Lang/Comp"])
 # open course in selenium
-#driver.get(final_query)
+# driver.get(final_query)
 
 def course_loop():
     while True:
+        # reset summative and formative grades for every run of program
+        summatives = []
+        formatives = []
         print(courses.keys())
         next_course = input('What course would you like to calculate next? (Type \'stop\' to stop)')
         if next_course == 'stop':
-            break
+            print('Goodbye!')
+            exit(0)
         else:
-            pass
+            final_query = 'https://campus.forsyth.k12.ga.us/campus/nav-wrapper/student/portal/student/classroom/grades/student-grades?' + funky_url_parameters + 'classroomSectionID=' + str(
+                courses[next_course])
+            driver.get(final_query)
+            calculate_grade_final(summatives, formatives, next_course)
+
 
 course_loop()
